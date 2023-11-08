4ef69951c0a4446460b2d9d1827cee8eb23924b5
Dragan Pilipovic <draganpilipovic1997bp@gmail.com>
2023-11-08 02:58:50 +0100

    run clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
-use ipa_multipoint::{*, multiproof::MultiPoint, lagrange_basis::LagrangeBasis};
-use std::*;
+use ipa_multipoint::{multiproof::MultiPoint, lagrange_basis::LagrangeBasis};
 use ark_ff::fields::PrimeField;
 use ipa_multipoint::crs::CRS;
 use banderwagon::multi_scalar_mul;
 use ark_serialize::CanonicalSerialize;
 use ipa_multipoint::lagrange_basis::PrecomputedWeights;
 use ipa_multipoint::transcript::Transcript;
-use std::time::{Instant, Duration};
 use ipa_multipoint::multiproof::ProverQuery;
 use ipa_multipoint::multiproof::VerifierQuery;
 use banderwagon::Fr;
@@ -69,11 +67,11 @@ fn exposed_pedersen_commit_to_fr(input: Vec<u8>) -> [u8;32] {
         for b in inp.chunks(32) {
             scalars.push(Fr::from_be_bytes_mod_order(b));
         }
-        
+
         // Committing all values at once.
         let bases = CRS::new(n_scalars, PEDERSEN_SEED);
         let commit = multi_scalar_mul(&bases.G, &scalars);
-    
+
         // Serializing via x/y in projective coordinates, to int and to scalars.
         let scalar = group_to_field(&commit);
         let mut scalar_bytes = [0u8; 32];
@@ -139,7 +137,7 @@ fn exposed_prove_call(input: Vec<u8>) -> Vec<u8> {
     let mut prover_queries: Vec<ProverQuery> = Vec::new();
 
 
-    for (i, chunk) in chunked_data.enumerate() {
+    for (_i, chunk) in chunked_data.enumerate() {
         if chunk.len() >= chunk_size {
             let data = chunk.clone();
             let commitment = Element::from_bytes(&data[0..32]).unwrap();
@@ -150,7 +148,7 @@ fn exposed_prove_call(input: Vec<u8>) -> Vec<u8> {
             let chunked_f_i_x_data = f_i_x.chunks(32);
 
             let mut collect_lagrange_basis: Vec<Fr> = Vec::new();
-            for (j, chunk_f_i_x) in chunked_f_i_x_data.enumerate() {
+            for (_j, chunk_f_i_x) in chunked_f_i_x_data.enumerate() {
                 if chunk_f_i_x.len() >= 32 {
                     let data_f_i_x = chunk_f_i_x.clone();
                     let fr_data_f_i_x = Fr::from_be_bytes_mod_order(&data_f_i_x);
@@ -199,8 +197,8 @@ fn exposed_verify_call(input: Vec<u8>) -> bool {
 
     let mut verifier_queries: Vec<VerifierQuery> = Vec::new();
 
-    for (i, chunk) in chunked_data.enumerate() {
-        let C_i = Element::from_bytes(&chunk[0..32]).unwrap();
+    for (_i, chunk) in chunked_data.enumerate() {
+        let c_i = Element::from_bytes(&chunk[0..32]).unwrap();
 
 
         let z_i: Fr = Fr::from(chunk[32] as u128);
@@ -208,7 +206,7 @@ fn exposed_verify_call(input: Vec<u8>) -> bool {
         let y_i = Fr::from_be_bytes_mod_order(&chunk[33..65]);
 
         let verifier_query = VerifierQuery {
-            commitment: C_i,
+            commitment: c_i,
             point: z_i,
             result: y_i,
         };
@@ -226,7 +224,7 @@ fn exposed_verify_call(input: Vec<u8>) -> bool {
     //TODO: process proof bytes
     let proof_bytes = [0u8; 256];
 
-    let mut proof = MultiPointProof::from_bytes(&proof_bytes, 256).unwrap();
+    let proof = MultiPointProof::from_bytes(&proof_bytes, 256).unwrap();
 
     let result = proof.check(&crs, &precomp, &verifier_queries,&mut transcript);
     result
@@ -246,14 +244,9 @@ pub(crate)fn group_to_field(point: &Element) -> Fr {
 
 #[test]
 fn test_exposed_pedersen_hash() {
-    let mut input = [167, 193, 134, 127, 101, 201, 125, 133, 93, 73, 253, 222, 238, 22, 188, 227, 193, 221, 183, 73, 85, 116, 113, 10, 172, 220, 152, 93, 7, 18, 133, 50, 176, 18, 222, 120, 221, 51, 160, 163, 54, 161, 15, 203, 174, 234, 12, 223, 129, 48, 85, 67, 27, 204, 93, 224, 124, 148, 116, 102, 156, 30, 114, 234];
+    let input = [167, 193, 134, 127, 101, 201, 125, 133, 93, 73, 253, 222, 238, 22, 188, 227, 193, 221, 183, 73, 85, 116, 113, 10, 172, 220, 152, 93, 7, 18, 133, 50, 176, 18, 222, 120, 221, 51, 160, 163, 54, 161, 15, 203, 174, 234, 12, 223, 129, 48, 85, 67, 27, 204, 93, 224, 124, 148, 116, 102, 156, 30, 114, 234];
 
-
-    let start = Instant::now();
     let result = exposed_pedersen_hash(input);
-    let elapsed = start.elapsed();
-    println!("Time elapsed: {:?}", elapsed);
-
 
     let expected = [0, 36, 192, 4, 159, 203, 95, 99, 186, 161, 2, 23, 116, 186, 110, 246, 122, 87, 32, 211, 218, 235, 31, 41, 158, 1, 37, 224, 151, 187, 39, 31];
     assert_eq!(result, expected);
@@ -263,7 +256,6 @@ fn test_exposed_pedersen_hash() {
 fn test_exposed_pedersen_commit_to_fr() {
     let input: [u8; 32] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
 
-    let start = Instant::now();
     let result = exposed_pedersen_commit_to_fr(input.to_vec());
 
     let bases = CRS::new(1, PEDERSEN_SEED);
@@ -274,7 +266,5 @@ fn test_exposed_pedersen_commit_to_fr() {
     let mut scalar_bytes = [0u8; 32];
     scalar.serialize(&mut scalar_bytes[..]).expect("could not serialise Fr into a 32 byte array");
     scalar_bytes.reverse();
-    let elapsed = start.elapsed();
-    println!("Time elapsed: {:?}", elapsed);
     assert_eq!(result, scalar_bytes);
 }
